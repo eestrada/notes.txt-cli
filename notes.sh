5c9e17201c3094252fa2ee65cb9cc02bb08329a7
Ethan Estrada <ethan.estrada@gmail.com>
2018-05-04 01:35:51 -0600

    Complete basics of `new` sub-command

--- notes.sh
+++ notes.sh
@@ -5,6 +5,29 @@
 # * https://github.com/nmeum/tpm/blob/master/tpm
 # * https://notabug.org/kl3/spm/src/master/spm
 
+NOTES_DIR="$(pwd)"
+ARCHIVE_DIR="${NOTES_DIR}/archive"
+
+# export TODO_DIR="${HOME}/Syncthing/Notes-txt"
+
+# echo "Globel config defaults"
+# echo "$NOTES_DIR"
+# echo "$ARCHIVE_DIR"
+
+if [ -e ".notestxtrc" ] ; then
+    . ".notestxtrc"
+    # echo "Local config"
+    # echo "$NOTES_DIR"
+    # echo "$ARCHIVE_DIR"
+elif [ -e "${HOME}/.notestxtrc" ] ; then
+    . "${HOME}/.notestxtrc"
+    # echo "Home dir config"
+    # echo "$NOTES_DIR"
+    # echo "$ARCHIVE_DIR"
+fi
+
+# exit 0
+
 abort() {
     printf '%s\n' "${1}" 1>&2
     exit 1
@@ -20,20 +43,25 @@ search() {
 }
 
 new() {
-    _tempfile=$(TMPDIR="$(pwd)" mktemp)
+    _tempfile=$(TMPDIR="${NOTES_DIR}" mktemp)
+
     xclip -o -selection clipboard > $_tempfile
     $EDITOR $_tempfile
 
-    # TODO: grab first line, strip trailing newline, trim it down to a
-    # reasonable length, translate illegal characters into something safe for
-    # filenames, then use this value as the filename.
-    # head -n 1 $_tempfile | tr -d '\n' | tr 'bad' 'good'
+    # NOTE: stripped out characters are loosely based on reserved characters
+    # mentioned here:
+    # https://en.wikipedia.org/wiki/Filename#Comparison_of_filename_limitations
 
-    ln $_tempfile $(uuidgen)-datehere.txt
+    # NOTE: <Get the first line> | <remove potentially illegal characters> |
+    # <make no longer than 60 characters> | <strip leading and trailing spaces>
+    _title="$(head -n 1 $_tempfile | tr -d '#$?%*:|()<>.,"/\n\\' | cut -c 1-60 | sed -E -e 's/^[ _-]*//' -e 's/[ _-]*$//')"
+    _full_title="${_title}-$(TZ=UTC date "+%Y%m%dT%H%M%SZ").txt"
 
-    # TODO: How to deal with the hardlink if it fails? Right now we
-    # unequivically unlink the old file.
+    ln $_tempfile "${_full_title}"
     unlink $_tempfile
+
+    test -f "${_full_title}"
+    return $?
 }
 
 PROGRAM="${0##*/}"
